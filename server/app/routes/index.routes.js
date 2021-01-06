5f38adbee22b88d5b937c67148da494ac8b4c3f8
fedorax <fedorax@hotmail.co.jp>
2021-01-06 23:15:56 +0900

    Update index.routes.js

--- server/app/routes/index.routes.js
+++ server/app/routes/index.routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 // Define the routes module' method
 module.exports = function(app) {
     // Catch all other routes and return the index file
-    app.get('/', (req, res) => {
+    app.get('/:path', (req, res) => {
         console.log(__dirname);
         res.sendFile(path.join(__dirname, '../../../dist/client/index.html'));
     });
